dfb398db4faaa5f826ad59ba9c31c980f544e8d9
Gustavo <lizarraga.gux@gmail.com>
2019-09-24 09:08:48 -0400

    Add new examples

---
+++ src/k_09_arrayYColecciones/Ejemplo5_1.kt
@@ -0,0 +1,21 @@
+package k_09_arrayYColecciones
+
+fun main() {
+    val rows = 2
+    val columns = 3
+    val firstMatrix = arrayOf(intArrayOf(2, 3, 4), intArrayOf(5, 2, 3))
+    val secondMatrix = arrayOf(intArrayOf(-4, 5, 3), intArrayOf(5, 6, 3))
+    val sum = Array(rows) { IntArray(columns) }
+    for (i in 0 until rows) {
+        for (j in 0 until columns) {
+            sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j]
+        }
+    }
+    println("Sum of two matrices is: ")
+    for (row in sum) {
+        for (column in row) {
+            print("$column    ")
+        }
+        println()
+    }
+}
